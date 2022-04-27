e71e5c50dc17309bede23c6b43bc8696d92d7b24
Erikazhuang <46492060+Erikazhuang@users.noreply.github.com>
2022-04-27 10:35:02 +0100

    example of using fluent api

---
+++ FluentApi/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using FluentApi.Entity;
+
+namespace FluentApi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            GeneratePlanRequest request = new GeneratePlanRequest.Builder()
+                                                                .withCountry("UK")
+                                                                .withComputationType("Refresh")
+                                                                .build();
+            Console.WriteLine(  request.ToString());
+        }
+    }
+}
